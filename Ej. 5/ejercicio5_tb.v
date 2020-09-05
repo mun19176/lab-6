0629d99a610b771bf94eaa88862f0e322b133a4f
mun19176 <68131418+mun19176@users.noreply.github.com>
2020-09-05 16:58:27 -0600

    Update ejercicio5_tb.v

--- Ej. 5/ejercicio5_tb.v
+++ Ej. 5/ejercicio5_tb.v
@@ -1,44 +1,43 @@
 module TB();
 
-  reg C, R, S;
-  reg [3:0] A;
-  wire [3:0] Y;
+  reg clc, Res, S;
+  reg [3:0] Y;
+  wire [3:0] Out;
 
 
-	FtypeD U1(C, A, R, S, Y);
+	FtypeD U1(clc, Y, Res, S, Out);
 
 
 initial begin
-	C=0;
-	forever #1 C = ~C;
+	clc=0;
+	forever #1 clc = ~clc;
 end
   initial begin
 	#5
-    $display("\n");
-	$display("Ejercicio 4, FlipFlop D");
-    $display("  Q    D  C   S");
+	$display("Ejercicio 5 ");
+    $display("  R    T  clc   S");
     $display("------------");
-    $monitor("%b %b %b %b", Y, A, C, S);
+    $monitor("%b %b %b %b", Out, Y, clc, S);
 
-	#1 S = 1; A = 4'b1100;
-	#1 S = 1; A = 4'b1100;
-	#1 S = 0; A = 4'b1100;
-	#1 S = 0; A = 4'b1100;
-       R = 0; A = 4'b0000;
-    #1 R = 0; A = 4'b1100;
-    #1 R = 0; A = 4'b1111;
-	#1 R = 0; A = 4'b1111;
-	#1 R = 0; A = 4'b0011;
-	#1 R = 0; A = 4'b0011;
-	#1 R = 0; A = 4'b1111;
-	#1 R = 1; A = 4'b1111;
-	#1 R = 0; A = 4'b1100;
+	#1 S = 1; Y = 4'b1100;
+	#1 S = 1; Y = 4'b1100;
+	#1 S = 0; Y = 4'b1100;
+	#1 S = 0; Y = 4'b1100;
+       Res = 0; Y = 4'b0000;
+    #1 Res = 0; Y = 4'b1100;
+    #1 Res = 0; Y = 4'b1111;
+	#1 Res = 0; Y = 4'b1111;
+	#1 Res = 0; Y = 4'b0011;
+	#1 Res = 0; Y = 4'b0011;
+	#1 Res = 0; Y = 4'b1111;
+	#1 Res = 1; Y = 4'b1111;
+	#1 Res = 0; Y = 4'b1100;
 
   end
 	initial
     #20 $finish;
   initial begin
-    $dumpfile("FlipFlopD_tb.vcd");
+    $dumpfile("Ejercicio5_tb.vcd");
     $dumpvars(0, TB);
   end
 endmodule
